10dd40ff8e496ec717312a0d303d54253053a0dc
Anton <bazhan.anton@ukr.net>
2020-10-15 18:27:20 +0300

    feat: add dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  return members
+    .filter((e) => typeof e === "string")
+    .map((e) => e.trim())
+    .map((e) => e[0].toUpperCase())
+    .sort()
+    .join("");
 };
