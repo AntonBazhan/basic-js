e68d6a4a07107169f2f82d16bcb3176d500b52a4
Anton <bazhan.anton@ukr.net>
2020-10-31 17:01:56 +0200

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  const array = [...arr];
+  array.map((element, index) => {
+    if (element === "--discard-next") {
+      // if (element.length < index) {
+      //   array.splice(index, 1);
+      // }
+      array.splice(index, 2, "delete");
+    } else if (element === "--discard-prev") {
+      if (index === 0) {
+        array.splice(0, 1);
+      } else {
+        array.splice(index - 1, 2, "delete");
+      }
+    } else if (element === "--double-prev") {
+      if (index === 0) {
+        array.splice(0, 1);
+      } else {
+        array.splice(index, 1, array[index - 1]);
+      }
+    } else if (element === "--double-next") {
+      if (array.length === index + 1) {
+        array.pop();
+      } else {
+        array.splice(index, 1, array[index + 1]);
+      }
+    }
+  });
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return array.filter((element) => element !== "delete");
 };
